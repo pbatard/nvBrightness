d7d8c12e68ce3afc67f9f542f23d159a8fa894b9
Pete Batard <pete@akeo.ie>
2025-08-02 19:53:01 +0100

    Add tray support and icons
    * Tray support from https://github.com/zserge/tray.
    * Icons partially generated with Axialis IconGenerator (

--- src/nvBrightness.c
+++ src/nvBrightness.c
@@ -27,31 +27,78 @@
 #include <windows.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "tray.h"
+
+// Globals
+static int8_t brightness = 75;
+static bool enabled = true;
+static bool use_alternate_keys = false;
+static struct tray tray;
+
+// Callbacks for Tray
+static void AlternateKeysCallback(struct tray_menu* item)
+{
+	use_alternate_keys = !use_alternate_keys;
+	OutputDebugStringA("Alternate Keys: ");
+	OutputDebugStringA(use_alternate_keys ? "Enabled\n" : "Disabled\n");
+	item->checked = !item->checked;
+	tray_update(&tray);
+}
+
+static void PauseCallback(struct tray_menu* item)
+{
+	enabled = !enabled;
+	item->checked = !item->checked;
+	tray_update(&tray);
+}
+
+static void ExitCallback(struct tray_menu* item)
+{
+	(void)item;
+	tray_exit();
+}
+
+// Keyboard procs
 static void ChangeBrightness(bool bIncrease)
 {
-	OutputDebugStringA(bIncrease ? "INCREASE\r\n" : "DECREASE\r\n");
+	static wchar_t icon_path[256];
+	static wchar_t menu_txt[64];
+
+	brightness += bIncrease ? +5 : -5;
+	if (brightness < 0)
+		brightness = 0;
+	if (brightness > 100)
+		brightness = 100;
+
+	swprintf(icon_path, ARRAYSIZE(icon_path), L"C:\\Projects\\nvBrightness\\icons\\%02d.ico", brightness);
+	swprintf(menu_txt, ARRAYSIZE(menu_txt), L"Brightness: %d%%", brightness);
+	tray.icon = icon_path;
+	tray.menu[0].text = menu_txt;
+	tray_update(&tray);
 }
 
 static LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if (nCode == HC_ACTION && lParam != 0) {
+	if (enabled && nCode == HC_ACTION && lParam != 0) {
 		KBDLLHOOKSTRUCT* kbhs = (KBDLLHOOKSTRUCT*)lParam;
 
-#ifndef USE_INTERNET_KEYS
-		if ((kbhs->vkCode == VK_SCROLL || kbhs->vkCode == VK_PAUSE) && (kbhs->flags == 0x0)) {
-			ChangeBrightness((kbhs->vkCode == VK_PAUSE));
-			return 1;
+		if (!use_alternate_keys) {
+			// TODO: Check if Alt/Ctr/Shift are pressed and ignore then?
+			if ((kbhs->vkCode == VK_SCROLL || kbhs->vkCode == VK_PAUSE) && (kbhs->flags == 0x0)) {
+				ChangeBrightness((kbhs->vkCode == VK_PAUSE));
+				return 1;
+			}
+		} else {
+			// Alternate way, that uses the Internet navigation keys
+			if ((kbhs->vkCode == VK_LEFT || kbhs->vkCode == VK_RIGHT) && ((kbhs->flags & 0xA1) == 0x21)) {
+				ChangeBrightness((kbhs->vkCode == VK_RIGHT));
+				return 1;
+			}
 		}
-#else
-		// Alternate way, that uses the Internet navigation keys
-		if ((kbhs->vkCode == VK_LEFT || kbhs->vkCode == VK_RIGHT) && ((kbhs->flags & 0xA1) == 0x21)) {
-			ChangeBrightness((kbhs->vkCode == VK_RIGHT));
-			return 1;
-		}
-#endif
 	}
 
 	return CallNextHookEx(NULL, nCode, wParam, lParam);
@@ -60,7 +107,23 @@ static LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lP
 int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	HHOOK hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, GetModuleHandle(0), 0);
-	MessageBox(NULL, L"HOOK RUNNING!", L"", MB_ICONEXCLAMATION | MB_SYSTEMMODAL);
+	static struct tray_menu menu[] = {
+		{.text = L"Brightness: 75%" },
+		{.text = L"Pause", .checked = 0, .cb = PauseCallback },
+		{.text = L"Use Internet Keys", .checked = 0, .cb = AlternateKeysCallback },
+		{.text = L"Exit", .cb = ExitCallback },
+		{.text = NULL }
+	};
+	tray.icon = L"C:\\Projects\\nvBrightness\\icons\\75.ico";
+	tray.menu = menu;
+
+	if (tray_init(&tray) < 0) {
+		OutputDebugStringA("Failed to create tray\n");
+		return 1;
+	}
+	while (tray_loop(1) == 0);
+
 	UnhookWindowsHookEx(hHook);
+
 	return 0;
 }
